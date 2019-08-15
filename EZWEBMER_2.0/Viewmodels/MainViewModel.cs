fe5d44b1b66f02e15ab4b41aac934100820ac7e2
NST069 <nislav.ns@gmail.com>
2019-08-15 13:25:10 +0300

    MVVM test project

--- EZWEBMER_2.0/Viewmodels/MainViewModel.cs
+++ EZWEBMER_2.0/Viewmodels/MainViewModel.cs
@@ -5,11 +5,31 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EZWEBMER_2._0.Viewmodels
 {
     class MainViewModel : INotifyPropertyChanged
     {
+        private int clicks;
+        public int Clicks {
+            get { return clicks; }
+            set {
+                clicks = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand Click {
+            get {
+                return new Models.DelegateCommand((obj) =>
+                {
+                    Clicks++;
+                }, (obj) => Clicks < 10);
+            }
+        }
+
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
