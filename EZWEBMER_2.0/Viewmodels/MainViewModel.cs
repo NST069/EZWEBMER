a24245d351a5627eba7d35f7b925bef3d2806664
NST069 <nislav.ns@gmail.com>
2019-08-15 11:44:27 +0300

    Init

---
+++ EZWEBMER_2.0/Viewmodels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZWEBMER_2._0.Viewmodels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]String info = "")
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
