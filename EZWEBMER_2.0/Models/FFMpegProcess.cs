fc0e2d7852c51b22bdd3cca8f4138d504e8ec671
NST069 <nislav.ns@gmail.com>
2019-08-16 19:59:38 +0300

    Checking files before starting Process

--- EZWEBMER_2.0/Models/FFMpegProcess.cs
+++ EZWEBMER_2.0/Models/FFMpegProcess.cs
@@ -9,22 +9,27 @@ namespace EZWEBMER_2._0.Models
 {
     static class FFMpegProcess
     {
-        public static void Start(String p, String m) {
-            String folder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName + @"\ffmpeg\";
-            String cmdtext = "/C cd " + folder + " & " +
-                "ffmpeg -loop 1 -r 1 "+
-                "-i \"" + p + "\" -i \"" + m + 
-                "\" -b:v 0 -crf 2 -b:a 160K -shortest -g 9999 "+
-                "-pix_fmt yuv420p -speed 0 -deadline 0 -threads 4 "+
-                "\"C:\\Users\\nisla\\Desktop\\test.webm\"";
-            System.Diagnostics.Process process = new System.Diagnostics.Process();//.Start("cmd.exe", cmdtext);
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            
-            startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = cmdtext;
-            process.StartInfo = startInfo;
-            process.Start();
+        public static void Start(String p, String m) { // clean required
+            if (!File.Exists(p)) System.Windows.MessageBox.Show("No Picture Found");
+            if (!File.Exists(m)) System.Windows.MessageBox.Show("No Music Found");
+            if (File.Exists(p) && File.Exists(m))
+            {
+                String folder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName + @"\ffmpeg\";
+                String cmdtext = "/C cd " + folder + " & " +
+                    "ffmpeg -loop 1 -r 1 " +
+                    "-i \"" + p + "\" -i \"" + m +
+                    "\" -b:v 0 -crf 2 -b:a 160K -shortest -g 9999 " +
+                    "-pix_fmt yuv420p -speed 0 -deadline 0 -threads 4 " +
+                    "\"C:\\Users\\nisla\\Desktop\\test.webm\"";
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = cmdtext;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
         }
     }
 }
